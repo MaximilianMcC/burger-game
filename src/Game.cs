b2e389bf83f74d972768aa38f5db1e53465988cc
MaximilianMcC <max@maximilian.co.nz>
2023-08-17 10:50:28 +1000

    Setup sfml

---
+++ src/Game.cs
@@ -0,0 +1,32 @@
+﻿using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+class Game
+{
+
+	public static RenderWindow Window { get; private set; }
+	public static float DeltaTime { get; private set; }
+
+	public void Run()
+	{
+		// Create the SFML window
+		Window = new RenderWindow(new VideoMode(800, 800), "Burger");
+		Window.SetFramerateLimit(60);
+		Window.Closed += (sender, e) => Window.Close();
+
+		// Clock and ui
+		Clock deltaTimeClock = new Clock();
+
+		while (Window.IsOpen)
+		{
+			// Handle events and whatnot
+			Window.DispatchEvents();
+			DeltaTime = deltaTimeClock.Restart().AsSeconds();
+
+			// Draw/render everything			
+			Window.Clear(Color.Magenta);
+			Window.Display();
+		}
+	}
+}
