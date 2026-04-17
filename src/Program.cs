0f2aa458995c435bf662dbe2dbfa79f090251bfd
MaximilianMcC <max@maximilian.co.nz>
2026-04-17 22:44:27 +1200

    restarting with a raylib upgrade

--- src/Program.cs
+++ src/Program.cs
@@ -1,8 +1,19 @@
-﻿class Program
+﻿using Raylib_cs;
+
+class Program
 {
 	public static void Main(string[] args)
 	{
-		Game game = new Game();
-		game.Run();
+		Raylib.SetTraceLogLevel(TraceLogLevel.Warning);
+		Raylib.InitWindow(800, 600, "rayib now lol");
+
+		while (Raylib.WindowShouldClose() == false)
+		{
+			Raylib.BeginDrawing();
+			Raylib.ClearBackground(Color.Magenta);
+			Raylib.EndDrawing();
+		}
+
+		Raylib.CloseWindow();
 	}
 }
--- src/Utils.cs
+++
@@ -1,8 +0,0 @@
-public static class Utils
-{
-	public static T RandomListElement<T>(List<T> list)
-	{
-		Random random = new Random();
-		return list[random.Next(0, list.Count)];
-	}
-}
