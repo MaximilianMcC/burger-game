677d068b7b00107ad9227da8bfd0fa4f0ca4b939
MaximilianMcC <max@maximilian.co.nz>
2026-04-18 00:42:14 +1200

    text is generated better

--- src/Receipt.cs
+++ src/Receipt.cs
@@ -22,8 +22,8 @@ class Receipt : GameObject
 		Vector2 position = Position;
 
 		Vector2 size = new Vector2(
-			Raylib.GetScreenWidth() / 4.5f,
-			Raylib.GetScreenHeight()
+			Raylib.GetScreenWidth() * 0.25f,
+			Raylib.GetScreenHeight() * 0.9f
 		);
 
 		// Draw the actual receipt
@@ -44,37 +44,72 @@ class Receipt : GameObject
 		position.Y += logoSize.Y;
 
 		// Make the order summary
-		string text = "ORDER SUMMARY ========\n";
-		text += "> TOP BUN        $0.10\n";
-		text += "> CHEESE         $0.10\n";
-		text += "> MEET           $1.20\n";
-		text += "> LETTUCE        $0.10\n";
-		text += "> TOMATO         $0.20\n";
-		text += "> BOTTOM BUN     $0.10\n";
-		text += "======================\n";
-		text += "AMOUNT DUE: $2.9\n";
-
-		text += "\n\n\n";
-		text += "Thanks for shopping at\n";
-		text += "Max Hambur, Next order is\n";
-		text += "10% off (not guarantee)";
+		string[] text = new string[]
+		{
+			"",
+			"ORDER SUMMARY ========",
+			"> TOP BUN        $0.10",
+			"> CHEESE         $0.10",
+			"> MEET           $1.20",
+			"> LETTUCE        $0.10",
+			"> TOMATO         $0.20",
+			"> BOTTOM BUN     $0.10",
+			"======================",
+			"AMOUNT DUE: $2.9",
+			"",
+			"",
+			"",
+			"Thanks for shopping at",
+			"Max Hambur.",
+			"",
+			"nga mihi"
+		};
 
 		// Draw the order summary thingy
 		position.X += 10f;
-		Raylib.DrawTextEx(
-			font,
-			text,
-			position,
-			11f,
-			11f / 10f,
-			Color.Black
-		);
-		position.Y += 11f * 15f;
+		DrawText(ref position, size, text);
 
 		// Draw the barcode
 		position.Y += 80;
 		logoSize.Y *= 0.3f;
-		DrawBarcode(position, logoSize, DateTimeOffset.UtcNow.ToUnixTimeSeconds());
+		DrawBarcode(position, logoSize, 1234567894564512312);
+	}
+
+	private void DrawText(ref Vector2 position, Vector2 size, string[] text)
+	{
+		// Figure out how large the font size must be
+		float fontSize;
+		float fontSpacing;
+		{
+			// Make a 'default' font to base all our calculations on
+			// TODO: Maybe measure with 'M' but I am pretty sure this is monospace anyways
+			//? also 22 is just the longest line thing yk 
+			// TODO: Don't hardcode 22
+			string testText = new String('=', 22);
+			const float defaultFontSize = 16f;
+			const float defaultFontSpacing = 10f / defaultFontSize;
+			float defaultFontWidth = Raylib.MeasureTextEx(font, testText, defaultFontSize, defaultFontSpacing).X;
+
+			// Calculate what the font size should be
+			// TODO: Don't do this 0.8 thing
+			float scale = size.X / defaultFontWidth;
+			fontSize = (defaultFontSize * scale) * 0.8f;
+			fontSpacing = 10f / fontSize;
+		}
+
+		// Loop over each line and print it
+		foreach (string line in text)
+		{
+			Raylib.DrawTextEx(
+				font,
+				line,
+				position,
+				fontSize,
+				fontSpacing,
+				Color.Black
+			);
+			position.Y += fontSize + fontSpacing;
+		}
 	}
 
 	private void DrawBarcode(Vector2 position, Vector2 size, long number)
